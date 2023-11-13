977b0ea636871504169eb3b3c3593d9099c4212f
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-13 18:38:46 +0900

    입력 방식 변경.

--- src/main/kotlin/christmas/InputView.kt
+++ src/main/kotlin/christmas/InputView.kt
@@ -8,7 +8,7 @@ class InputView {
         println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
         while (true) {
             try {
-                return LocalDate.of(2023, 12, readLine()!!.toInt())
+                return LocalDate.of(2023, 12, Console.readLine()!!.toInt())
             } catch (e: Exception) {
                 println("[ERROR] 날짜는 숫자만 입력해 주세요.")
             }
@@ -19,7 +19,7 @@ class InputView {
         println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
         while (true) {
             try {
-                val input = readLine()!!
+                val input = Console.readLine()!!
                 val order = parseOrder(input, date)
                 return order
             } catch (e: IllegalArgumentException) {
