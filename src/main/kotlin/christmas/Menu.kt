1bd257383192f5b6f6f8739d28522f11b02c69fb
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-13 14:26:29 +0900

    메뉴 - 음식가격 설정 코드보완

--- src/main/kotlin/christmas/Menu.kt
+++ src/main/kotlin/christmas/Menu.kt
@@ -3,26 +3,35 @@ package christmas
 data class Menu(val name: String, val price: Int)
 
 class MenuList {
-    private val appetizers = mutableListOf<Menu>()
-    private val mains = mutableListOf<Menu>()
-    private val desserts = mutableListOf<Menu>()
-    private val beverages = mutableListOf<Menu>()
+    companion object {
+        // 메뉴의 이름과 가격 정보를 상수로 선언합니다.
+        private val APPETIZERS = listOf(
+            Menu("양송이수프", 6000),
+            Menu("타파스", 5500),
+            Menu("시저샐러드", 8000)
+        )
 
-    init {
-        appetizers.add(Menu("양송이수프", 6000))
-        appetizers.add(Menu("타파스", 5500))
-        appetizers.add(Menu("시저샐러드", 8000))
+        private val MAINS = listOf(
+            Menu("티본스테이크", 55000),
+            Menu("바비큐립", 54000),
+            Menu("해산물파스타", 35000),
+            Menu("크리스마스파스타", 25000)
+        )
 
-        mains.add(Menu("티본스테이크", 55000))
-        mains.add(Menu("바비큐립", 54000))
-        mains.add(Menu("해산물파스타", 35000))
-        mains.add(Menu("크리스마스파스타", 25000))
+        private val DESSERTS = listOf(
+            Menu("초코케이크", 15000),
+            Menu("아이스크림", 5000)
+        )
 
-        desserts.add(Menu("초코케이크", 15000))
-        desserts.add(Menu("아이스크림", 5000))
-
-        beverages.add(Menu("제로콜라", 3000))
-        beverages.add(Menu("레드와인", 60000))
-        beverages.add(Menu("샴페인", 25000))
+        private val BEVERAGES = listOf(
+            Menu("제로콜라", 3000),
+            Menu("레드와인", 60000),
+            Menu("샴페인", 25000)
+        )
     }
+
+    private val appetizers = APPETIZERS.toMutableList()
+    private val mains = MAINS.toMutableList()
+    private val desserts = DESSERTS.toMutableList()
+    private val beverages = BEVERAGES.toMutableList()
 }
