f0467123da6cc1308ec722080647a29068675f4b
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-13 15:25:00 +0900

    메인 함수

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,24 @@
 package christmas
 
+import camp.nextstep.edu.missionutils.Console
 fun main() {
-    TODO("프로그램 구현")
+    val inputView = InputView()
+    val outputView = OutputView()
+    val event = Event()
+
+    try {
+        val date = inputView.readDate()
+        val order = inputView.readOrder()
+
+        val badge = event.applyEvent(order)
+
+        outputView.printMenu(order)
+        // TODO: 출력 로직을 구현
+        outputView.printBadge(badge)
+
+    } catch (e: IllegalArgumentException) {
+        println("[ERROR] ${e.message}")
+        // 잘못된 입력이 있을 경우, 해당 부분부터 입력을 다시 받습니다.
+        main()
+    }
 }
