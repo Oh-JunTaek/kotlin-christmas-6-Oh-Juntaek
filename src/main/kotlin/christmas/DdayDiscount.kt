eef5dca220b53316da777614cdcbc036818715ad
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-13 14:29:46 +0900

    이벤트 날짜 코드 보완

--- src/main/kotlin/christmas/DdayDiscount.kt
+++ src/main/kotlin/christmas/DdayDiscount.kt
@@ -6,21 +6,20 @@ class DdayDiscount {
     companion object {
         const val START_DISCOUNT = 1000
         const val INCREASE_DISCOUNT = 100
+        // 이벤트 시작 및 종료 날짜를 상수로 선언합니다.
+        val START_DATE = LocalDate.of(2023, 12, 1)
+        val END_DATE = LocalDate.of(2023, 12, 25)
     }
-//할인 금액 상수 선언
-
-    private val startDate = LocalDate.of(2023, 12, 1)
-    private val endDate = LocalDate.of(2023, 12, 25)
 //이벤트 시작 및 종료 날짜를 LocalDate 객체로 선언
+
     fun calculateDiscount(date: LocalDate): Int {
         validateDate(date)
         return START_DISCOUNT + (date.dayOfMonth - 1) * INCREASE_DISCOUNT
     }
-//입력받은 날짜 확인 후 할인금액 계산
+
     private fun validateDate(date: LocalDate) {
-        if (date.isBefore(startDate) || date.isAfter(endDate)) {
-            throw IllegalArgumentException("날짜는 2023년 12월 1일부터 2023년 12월 25일까지만 가능합니다.")
+        if (date.isBefore(START_DATE) || date.isAfter(END_DATE)) {
+            throw IllegalArgumentException("날짜는 ${START_DATE.year}년 ${START_DATE.monthValue}월 ${START_DATE.dayOfMonth}일부터 ${END_DATE.year}년 ${END_DATE.monthValue}월 ${END_DATE.dayOfMonth}일까지만 가능합니다.")
         }
     }
 }
-//날짜 유효성 검사
