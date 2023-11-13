4415af23d137c149a642a957e96a36077b0edcd1
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-13 14:41:16 +0900

    이벤트 코드 작성

---
+++ src/main/kotlin/christmas/Event.kt
@@ -0,0 +1,14 @@
+package christmas
+
+class Event {
+    companion object {
+        const val MIN_ORDER_AMOUNT = 10000
+        const val MAX_MENU_COUNT = 20
+        val BADGE_THRESHOLDS = mapOf(
+            "별" to 5000,
+            "트리" to 10000,
+            "산타" to 20000
+        )
+    }
+    // 상수 선언
+}
