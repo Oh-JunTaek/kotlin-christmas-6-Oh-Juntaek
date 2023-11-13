0d9b0996bbdcbfdd0ab50dbe77cdebc872c2505e
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-13 17:45:40 +0900

    메인 함수 보완

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,7 @@
 package christmas
 
+import java.time.LocalDate
+
 
 fun main() {
     println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
@@ -9,21 +11,30 @@ fun main() {
     val discount = DdayDiscount()
     val event = Event()
 
-    try {
-        val date = inputView.readDate()
-        val order = inputView.readOrder()
-        order.printOrder()
+    var date: LocalDate? = null
+    var order: Order? = null
 
-        val badge = event.applyEvent(order)
-        val totalDiscount = discount.calculateDiscount(order.date, order.totalAmount, order.isSpecialDay, order.dessertCount, order.mainCount)
-        val finalAmount = order.totalAmount - totalDiscount
-
-        outputView.printOrderSummary(order, discount, finalAmount)
-        outputView.printBadge(badge)
-
-    } catch (e: IllegalArgumentException) {
-        println("[ERROR] ${e.message}")
-        // 잘못된 입력이 있을 경우, 해당 부분부터 입력을 다시 받습니다.
-        main()
+    while (date == null) {
+        try {
+            date = inputView.readDate()
+        } catch (e: IllegalArgumentException) {
+            println("[ERROR] ${e.message}")
+        }
     }
+
+    while (order == null) {
+        try {
+            order = inputView.readOrder()
+            order.printOrder()
+        } catch (e: IllegalArgumentException) {
+            println("[ERROR] ${e.message}")
+        }
+    }
+
+    val badge = event.applyEvent(order)
+    val totalDiscount = discount.calculateDiscount(order.date, order.totalAmount, order.isSpecialDay, order.dessertCount, order.mainCount)
+    val finalAmount = order.totalAmount - totalDiscount
+
+    outputView.printOrderSummary(order, discount, finalAmount)
+    outputView.printBadge(badge)
 }
