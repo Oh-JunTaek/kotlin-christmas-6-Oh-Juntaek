f9811496e95fe96f2f70d0456cb59bf184536a8b
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-13 14:42:33 +0900

    이벤트 코드 작성 -주문

--- src/main/kotlin/christmas/Event.kt
+++ src/main/kotlin/christmas/Event.kt
@@ -1,5 +1,7 @@
 package christmas
 
+import org.junit.jupiter.api.Order
+
 class Event {
     companion object {
         const val MIN_ORDER_AMOUNT = 10000
@@ -19,4 +21,16 @@ class Event {
     }
 
 
+    private fun validateOrder(order: Order) {
+        if (order.totalAmount < MIN_ORDER_AMOUNT) {
+            throw IllegalArgumentException("총주문 금액은 ${MIN_ORDER_AMOUNT}원 이상이어야 합니다.")
+        }
+        if (order.totalCount > MAX_MENU_COUNT) {
+            throw IllegalArgumentException("메뉴는 한 번에 최대 ${MAX_MENU_COUNT}개까지만 주문할 수 있습니다.")
+        }
+        if (order.beverageCount == order.totalCount) {
+            throw IllegalArgumentException("음료만 주문할 수 없습니다.")
+        }
+    }
+    //주문
 }
